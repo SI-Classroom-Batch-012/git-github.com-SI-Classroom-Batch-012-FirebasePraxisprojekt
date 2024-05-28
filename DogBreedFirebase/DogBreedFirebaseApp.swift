88d9f10684d2b4fa5d1d2fdadce5f6c1ba25070b
markus <mgabler81@ahoo.com>
2024-05-28 13:23:44 +0200

    Initial Commit

---
+++ DogBreedFirebase/DogBreedFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedFirebaseApp.swift
+//  DogBreedFirebase
+//
+//  Created by Markus on 28.05.24.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
